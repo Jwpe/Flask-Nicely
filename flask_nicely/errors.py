e577a5b38fa13a0e344db9e7b5c6b40553c5da2c
Jwpe <jwpevans@gmail.com>
2013-10-27 15:42:15 -0500

    Added several additional HTTP errors. Changed the default error message behaviour.

--- flask_nicely/errors.py
+++ flask_nicely/errors.py
@@ -2,12 +2,37 @@
 class ErrorResponse(Exception):
 
     status = "500"
+    error_message = "Error"
 
     def __init__(self, error_message=None, **kwargs):
 
         Exception.__init__(self)
-        self.error_message = error_message or "Error"
+        self.error_message = error_message or self.error_message
+
+class Unauthorized(ErrorResponse):
+
+    status = "401"
+    error_message = "Unauthorized"
+
+
+class Forbidden(ErrorResponse):
+
+    status = "403"
+    error_message = "Forbidden"
+
 
 class NotFound(ErrorResponse):
 
     status = "404"
+    error_message = "Not Found"
+
+
+class ServerError(ErrorResponse):
+
+    error_message = "Internal Server Error"
+
+
+class GatewayTimeout(ErrorResponse):
+
+    status = "504"
+    error_message = "Gateway Timeout"
